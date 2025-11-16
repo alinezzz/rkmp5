09156dbd3853767cdfdbf8746e252182a3068bfb
alinezzz <161722152+alinezzz@users.noreply.github.com>
2025-11-16 22:46:58 +0300

    Стабильная версия

---
+++ lib/feature/horse_tour/screens/tour_details_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../models/horse_tour_model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class TourDetailsScreen extends StatelessWidget {
+  final TourModel tour;
+
+  const TourDetailsScreen({super.key, required this.tour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(tour.name)),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            CachedNetworkImage(
+              imageUrl: tour.pictureLink,
+              height: 250,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+            const SizedBox(height: 20),
+            Text(tour.name, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 12),
+            Text(tour.description, style: const TextStyle(fontSize: 16)),
+            const SizedBox(height: 24),
+                      ],
+        ),
+      ),
+    );
+  }
+}
